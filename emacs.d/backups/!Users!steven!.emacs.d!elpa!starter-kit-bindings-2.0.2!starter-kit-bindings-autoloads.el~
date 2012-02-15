1f62d7503aacc0d526d84c8a1b0c0fc6d55879a0
Steven Deobald <steven@c42.in>
2012-02-15 16:09:49 +0530

    [sd] osx emacs keybinding for meta, delete /backups

--- emacs.d/backups/!Users!steven!.emacs.d!elpa!starter-kit-bindings-2.0.2!starter-kit-bindings-autoloads.el~
+++
@@ -1,13 +0,0 @@
-;;; starter-kit-bindings-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-(provide 'starter-kit-bindings-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; starter-kit-bindings-autoloads.el ends here
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -85,9 +85,19 @@
     (require 'color-theme-wombat)
     (color-theme-wombat)))
 
+;; OSX SPECIFIC THINGS
+;;--------------------------------------------------
+
+;; fix the command key to be meta
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
 ;; KEYBINDINGS
 ;;--------------------------------------------------
 
+(global-set-key [f6] 'magit-status)
 (global-set-key [f7] 'ns-toggle-fullscreen)
 
 ;; steve yegges's suggested keybindings
